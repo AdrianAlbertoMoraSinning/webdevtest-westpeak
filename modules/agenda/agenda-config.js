2d78d0247573985085dc398cc9c90348e95b27d7
Adrian Alberto Mora Sinning <43191656+AdrianAlbertoMoraSinning@users.noreply.github.com>
2026-06-04 11:29:04 -0600

    Update agenda configuration for new company details

--- modules/agenda/agenda-config.js
+++ modules/agenda/agenda-config.js
@@ -3,16 +3,16 @@
   Change only this file when installing the agenda in another website.
 */
 window.AGENDA_CONFIG = {
-  companyName: "AP Abogados Asociados",
-  ownerEmail: "abogadosasociadosap4@mail.com",
-  ownerWhatsapp: "573108030751",
-  adminPin: "3108030751",
+  companyName: "Montecristo Mortgage + Auto",
+  ownerEmail: "marlon@mtcautofinance.ca",
+  ownerWhatsapp: "14039692916",
+  adminPin: "MTC2026",
   appsScriptUrl: "https://script.google.com/macros/s/AKfycbwjxYqU1jBtczlPz7-ONbvfOdPr25BT4vcYq3T8E9T9fyMLYw3axrmY0N1AFQk0srVF/exec",
   workDays: [1, 2, 3, 4, 5],
   startHour: 8,
   endHour: 17,
   slotMinutes: 60,
   maxDaysAhead: 45,
-  timezoneLabel: "hora local",
-  serviceLabel: "Consulta jurídica"
+  timezoneLabel: "Mountain Time",
+  serviceLabel: "Auto Financing Consultation"
 };
