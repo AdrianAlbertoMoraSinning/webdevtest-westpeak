af1ba375a41cf464aef80157b6c779350a7112d1
Adrian Alberto Mora Sinning <43191656+AdrianAlbertoMoraSinning@users.noreply.github.com>
2026-06-04 12:20:34 -0600

    Change appsScriptUrl in agenda-config.js
    Updated the appsScriptUrl to a new endpoint.

--- modules/agenda/agenda-config.js
+++ modules/agenda/agenda-config.js
@@ -1,13 +1,9 @@
-/*
-  Agenda reusable configuration
-  Change only this file when installing the agenda in another website.
-*/
 window.AGENDA_CONFIG = {
   companyName: "Montecristo Mortgage + Auto",
   ownerEmail: "marlon@mtcautofinance.ca",
   ownerWhatsapp: "14039692916",
   adminPin: "MTC2026",
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbwjxYqU1jBtczlPz7-ONbvfOdPr25BT4vcYq3T8E9T9fyMLYw3axrmY0N1AFQk0srVF/exec",
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzlgECdd5J2agPILr72ZR2nxYL7DUwygS0yznxGR1yR9acYMyTd_xSPHiX8STl8wplF/exec",
   workDays: [1, 2, 3, 4, 5],
   startHour: 8,
   endHour: 17,
